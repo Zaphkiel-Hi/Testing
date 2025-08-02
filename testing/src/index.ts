f0f2bd14ae27b9b5c4bde1a678f0112641d04fe1
Zaphkiel Hi <hirithik20@gmail.com>
2025-08-02 23:45:46 +0800

    save before pulling

--- testing/src/index.ts
+++ testing/src/index.ts
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
+dotenv.config({ path: './src/.env' });
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
