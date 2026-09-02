54340417c39a5d56055f85939ac43feb698177dd
Hirithik <74068473+Zaphkiel-Hi@users.noreply.github.com>
2026-09-02 07:18:35 +0530

    Update index.ts

--- testing/src/index.ts
+++ testing/src/index.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 
 // Simulate __dirname
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename); 
 
 // Load environment variables
 dotenv.config({ path: '.env' });
